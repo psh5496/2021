6769daca796077a256dc85ab14e115dc986fe5fa
seonghyeon <shp35079@naver.com>
2021-04-13 23:21:56 +0900

    [FIX]

--- myDiario/darkMode.js
+++ myDiario/darkMode.js
@@ -1,17 +1,29 @@
 const body = document.getElementsByTagName("body");
 const light = document.querySelector("#light");
-const yet = document.querySelector("#yet");
+const dark = document.querySelector("#dark");
 
 light.style.opacity = 1;
-light.addEventListener('click', function() {
-    if(light.style.opacity == 1) {
-        light.style.opacity = 0;
-        body[0].style.backgroundColor = "#302F2F";
-        yet.style.color = "#DCD6F7";
-    }
-    else {
-        light.style.opacity = 1;
-        body[0].style.backgroundColor = "#FFFFFF";
-        yet.style.color = "#424874";
-    }
-})
+
+const setMode = () => {
+  if (localStorage.getItem("mode") == "black") {
+    light.style.opacity = 0;
+    body[0].style.backgroundColor = "#302F2F";
+  } else {
+    light.style.opacity = 1;
+    body[0].style.backgroundColor = "#FFFFFF";
+  }
+};
+
+setMode();
+
+light.addEventListener("click", function () {
+  if (light.style.opacity == 1) {
+    light.style.opacity = 0;
+    body[0].style.backgroundColor = "#302F2F";
+    localStorage.setItem("mode", "black");
+  } else {
+    light.style.opacity = 1;
+    body[0].style.backgroundColor = "#FFFFFF";
+    localStorage.setItem("mode", "white");
+  }
+});
